74bc143585da8d2eb1c59959196e63b04e27b9ca
abooollfazl <abooollfazl@gmail.com>
2026-02-16 15:40:25 +0330

    Update BoolToColorConverter.cs

--- Converters/BoolToColorConverter.cs
+++ Converters/BoolToColorConverter.cs
@@ -3,19 +3,19 @@ using System.Globalization;
 namespace FoodApp.Converters
 {
     public class BoolToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
-                public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-                        {
-                                    if (value is bool boolValue)
-                                                {
-                                                                return boolValue ? Colors.Green : Colors.Red;
-                                                                            }
-                                                                                        return Colors.Gray;
-                                                                                                }
+            if (value is bool boolValue)
+            {
+                return boolValue ? Colors.Green : Colors.Red;
+            }
+            return Colors.Gray;
+        }
 
-                                                                                                        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-                                                                                                                {
-                                                                                                                            throw new NotImplementedException();
-                                                                                                                                    }
-                                                                                                                                        }
-                                                                                                                                        }
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
